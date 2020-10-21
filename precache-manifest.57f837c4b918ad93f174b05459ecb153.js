ab045e39279d37c6bd4719d7024d1f96a13ec9f9
Artem <a.kosherenkov@gmail.com>
2020-10-21 23:08:32 +0300

    Updates

---
+++ precache-manifest.57f837c4b918ad93f174b05459ecb153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54d93fcd41d4613deb98a037f5cd152",
+    "url": "/kosherenkov/notes/index.html"
+  },
+  {
+    "revision": "5be312344084f26e9da6",
+    "url": "/kosherenkov/notes/static/css/main.6255ca5a.chunk.css"
+  },
+  {
+    "revision": "605a7d95129ce830d0a7",
+    "url": "/kosherenkov/notes/static/js/2.032fb0f5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/kosherenkov/notes/static/js/2.032fb0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be312344084f26e9da6",
+    "url": "/kosherenkov/notes/static/js/main.b539669f.chunk.js"
+  },
+  {
+    "revision": "728d071f72f1f389a88f",
+    "url": "/kosherenkov/notes/static/js/runtime-main.45452e1a.js"
+  }
+]);
